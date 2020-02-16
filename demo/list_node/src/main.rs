089346e5200976aedea4137f7ca4c312ec624d55
chenshenhai <cshenhai@gmail.com>
2020-02-16 15:58:17 +0800

    docs: upadte readme.md

--- demo/list_node/src/main.rs
+++ demo/list_node/src/main.rs
@@ -1,16 +1,16 @@
 
 // 链表节点
 #[derive(PartialEq, Eq, Clone, Debug)]
-struct Node {
+struct ListNode {
     pub value: i32,
-    pub next: Option<Box<Node>>
+    pub next: Option<Box<ListNode>>
 }
 
 // 链表
-impl Node {
+impl ListNode {
     #[inline]
     fn new(value: i32) -> Self {
-        Node {
+        ListNode {
             value: value,
             next: None
         }
@@ -18,14 +18,14 @@ impl Node {
 }
 
 // 创建链表
-fn create_list(start: i32, count: i32, unit: i32) -> Option<Box<Node>> {
-    let mut head_node = Some(Box::new(Node::new(start)));
+fn create_list(start: i32, count: i32, unit: i32) -> Option<Box<ListNode>> {
+    let mut head_node = Some(Box::new(ListNode::new(start)));
     let mut prev_node = &mut head_node;
-    for i in 0..count as usize {
+    for i in 1..count as usize {
         match prev_node {
             Some(node) => {
                 let idx = (i + 1) as i32;
-                node.next = Some(Box::new(Node::new(idx * unit)));
+                node.next = Some(Box::new(ListNode::new(idx * unit)));
                 prev_node = &mut node.next;
             },
             None => {},
@@ -34,7 +34,26 @@ fn create_list(start: i32, count: i32, unit: i32) -> Option<Box<Node>> {
     head_node
 }
 
+// 遍历读取链表节点
+fn read_list (list: Option<Box<ListNode>>) {
+    let mut list = list;
+    let mut node = &mut list;
+    while node.is_some() {
+        println!("{}", node.as_ref().unwrap().value);
+
+        if node.is_some() {
+            node = &mut node.as_mut().unwrap().next;
+        } else {
+            break;
+        }
+    }
+}
+
 fn main() {
     let list = create_list(1, 4, 1);
+    println!("println list format: ");
     println!("{:?}", list);
+
+    println!("println all nodes of list: ");
+    read_list(list);
 }
