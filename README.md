a4ec488b9b0785f0fa6b9bedc37f367c564d6e37
chenshenhai <cshenhai@gmail.com>
2021-12-19 23:18:15 +0800

    feat: update demo/http_file_server

--- README.md
+++ README.md
@@ -19,6 +19,7 @@
 **数据结构:** [链表实现](./demo/list_node/src/main.rs)
 
 
- **设计模式:** [单例模式](./demo/pattern_singleton/) | [工厂模式](./demo/pattern_factory/)
+ **设计模式:** [单例模式](./demo/pattern_singleton/src/main.rs) | [工厂模式](./demo/pattern_factory/src/main.rs) | [创造者模式](./demo/pattern_builder/src/main.rs)
+
 
 
--- demo/http_file_server/src/main.rs
+++ demo/http_file_server/src/main.rs
@@ -1,9 +1,7 @@
-use chunked_transfer;
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 use std::thread;
 use std::fs::File;
-use chunked_transfer::Encoder;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3001").unwrap();
@@ -36,33 +34,37 @@ fn get_path(mut stream: &TcpStream) -> String {
 }
 
 fn response(path: &str, mut stream: TcpStream) {
-    let file_path: &str = &("./image".to_string() + path);
+    let file_path: &str = &("./files".to_string() + path);
     println!("{}", file_path);
     let mut buf = Vec::new();
     let file = File::open(file_path);
-    // file.unwrap().read_to_end(&mut buf);
 
     match file.unwrap().read_to_end(&mut buf) {
         Ok(_) => println!("Read file ok"),
         Err(e) => println!("Failed readinf file: {}", e),
     }
+    
+    // println!("buf = {:?}", buf);
+    let chunks = read_file_chunks(&buf, 8);
+    // println!("chunks = {:?}", chunks);
+    let mut headers = vec![
+        "HTTP/1.1 200 OK",
+    ];
 
-    let mut encoded: Vec<u8> = vec![];
-    {
-        let mut encoder = Encoder::with_chunks_size(&mut encoded, 8);
-        // encoder.write_all(&buf);
-        match encoder.write_all(&buf) {
-            Ok(_) => println!("write_all ok"),
-            Err(e) => println!("Failed write_all {}", e),
-        }
+    if path.ends_with(".html") {
+        headers.push("Content-type: text/html")
+    } else if path.ends_with(".png") {
+        headers.push("Content-type: image/png")
+    } else {
+        headers.push("Content-type: text/plain")
     }
+    headers.push("Transfer-Encoding: chunked");
+    headers.push("\r\n");
 
-    let headers =
-        ["HTTP/1.1 200 OK", "Content-type: image/png", "Transfer-Encoding: chunked", "\r\n"];
     let mut response: Vec<u8> = headers.join("\r\n")
         .to_string()
         .into_bytes();
-    response.extend(encoded);
+    response.extend(chunks);
 
     match stream.write(&response) {
         Ok(_) => println!("Response sent"),
@@ -72,4 +74,45 @@ fn response(path: &str, mut stream: TcpStream) {
 
 fn handle_client(stream: TcpStream) {
     response(&get_path(&stream), stream);
+}
+
+
+fn read_file_chunks(buf: &Vec<u8>, size: usize) -> Vec<u8> {
+    let size_code: u8 = (size + 48) as u8;
+    let r_code = 13;
+    let n_code = 10;
+    let mut _rest = buf.len() % size;
+    let mut chunks: Vec<u8> = vec![];
+    chunks.push(size_code);
+    chunks.push(r_code);
+    chunks.push(n_code);
+    let mut i = 0;
+    while i < buf.len() { 
+        _rest = i % size;
+        if _rest == 0 && i >= size as usize  {
+            if buf.len() - i <= size as usize {
+                chunks.push(r_code);
+                chunks.push(n_code);
+                chunks.push((buf.len() - i) as u8 + 48);
+                chunks.push(r_code);
+                chunks.push(n_code);
+            } else {
+                chunks.push(r_code);
+                chunks.push(n_code);
+                chunks.push(size_code);
+                chunks.push(r_code);
+                chunks.push(n_code);
+            } 
+        }
+        chunks.push(buf[i]);
+        i += 1;
+    }
+    chunks.push(r_code);
+    chunks.push(n_code);
+    chunks.push(48);
+    chunks.push(r_code);
+    chunks.push(n_code);
+    chunks.push(r_code);
+    chunks.push(n_code);
+    return chunks;
 }
