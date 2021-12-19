cf899dc3b7b7c38982fe04e0e8e9a786f481f2a8
chenshenhai <cshenhai@gmail.com>
2021-12-19 15:02:30 +0800

    feat: add demo/pattern_builder

---
+++ demo/pattern_builder/src/main.rs
@@ -0,0 +1,40 @@
+use std::fmt::Debug;
+
+#[derive(Debug)]
+pub struct Car {
+    name: String,
+    time: String,
+}
+
+pub struct CarBuilder {
+    time: String,
+}
+
+impl CarBuilder {
+    pub fn new() -> Self {
+        Self {
+            time: String::from("0000-00-00 00:00:00")
+        }
+    }
+
+    pub fn set_time(mut self, time: String) -> CarBuilder {
+        self.time = time;
+        self
+    }
+
+    pub fn build(&self, name: String) -> Car {
+        let car = Car {
+            time: self.time.to_string(),
+            name,
+        };
+        car
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+    let car = CarBuilder::new()
+        .set_time(String::from("2021-01-01: 00:00:01"))
+        .build(String::from("HelloCar"));
+    println!("car = {:?}", car);
+}
